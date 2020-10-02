35c5645eb4d9d2f1cb6dbf52ec7228b53611924d
EduSantosBrito <edu.santos.brito@gmail.com>
2020-10-02 11:10:05 -0300

    feat: create Header component

---
+++ components/Header/style.tsx
@@ -0,0 +1,39 @@
+import { WithTheme } from 'shared/withTheme';
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    &::before {
+        content: '';
+        background: url('/dot.svg');
+        background-repeat: repeat;
+        display: block;
+        width: 40vw;
+        max-width: 300px;
+        height: 70px;
+        position: absolute;
+        left: 0;
+        bottom: -50px;
+    }
+`;
+
+type HeroTextProps = WithTheme;
+
+export const HeroText = styled.p<HeroTextProps>`
+    font-weight: 600;
+    font-size: ${({ theme }: HeroTextProps) => theme.fontSizes.extraLarge};
+    color: ${({ theme }: HeroTextProps) => theme.colors.white};
+`;
+
+type HeroTextOrangeProps = WithTheme;
+
+export const HeroTextOrange = styled.span<HeroTextOrangeProps>`
+    font-weight: 600;
+    font-size: ${({ theme }: HeroTextProps) => theme.fontSizes.extraLarge};
+    color: ${({ theme }: HeroTextOrangeProps) => theme.colors.secondary};
+`;
