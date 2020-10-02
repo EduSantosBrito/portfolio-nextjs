6f4760b7d99b326cad24f89e69283b70d4a17384
EduSantosBrito <edu.santos.brito@gmail.com>
2020-10-02 20:39:43 -0300

    chore: create SectionTitle

---
+++ shared/SectionTitle.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { WithTheme } from './withTheme';
+
+type SectionTitleProps = WithTheme;
+
+export const SectionTitle = styled.h2<SectionTitleProps>`
+    font-weight: 600;
+    color: ${({ theme }: SectionTitleProps) => theme.colors.white};
+    font-size: ${({ theme }: SectionTitleProps) => theme.fontSizes.large};
+    text-align: center;
+    display: inline-block;
+    position: relative;
+    z-index: 2;
+
+    &::after {
+        content: '';
+        width: 100%;
+        height: 1px;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 2px;
+        background-color: ${({ theme }: SectionTitleProps) => theme.colors.secondary};
+        transition: height 500ms;
+        z-index: -1;
+    }
+
+    &:hover {
+        &::after {
+            height: 60%;
+        }
+    }
+`;
