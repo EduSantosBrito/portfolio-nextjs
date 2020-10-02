2373f7341434c92249c2c71f6c63ee4c8f144883
EduSantosBrito <edu.santos.brito@gmail.com>
2020-10-02 09:44:13 -0300

    feat: mobile menu

--- components/Header/layout.tsx
+++ components/Header/layout.tsx
@@ -1,6 +1,6 @@
 import React, { Dispatch, SetStateAction } from 'react';
 import { Logo } from 'shared/Logo';
-import { HeaderContainer, MenuIcon } from './style';
+import { ButtonLink, HeaderContainer, MenuIcon, NavList } from './style';
 
 interface HeaderLayoutProps {
     open: boolean;
@@ -9,9 +9,23 @@ interface HeaderLayoutProps {
 
 const HeaderLayout = ({ open, onNavClick }: HeaderLayoutProps) => {
     return (
-        <HeaderContainer open={open}>
+        <HeaderContainer id='header' open={open}>
             <Logo dark={!open} />
             <MenuIcon onClick={() => onNavClick(!open)} open={open} />
+            <NavList open={open}>
+                <a onClick={() => open && onNavClick(!open)} href='#header'>
+                    Home
+                </a>
+                <a onClick={() => open && onNavClick(!open)} href='#whatIdo'>
+                    What I do
+                </a>
+                <a onClick={() => open && onNavClick(!open)} href='#experiences'>
+                    Experiences
+                </a>
+            </NavList>
+            <ButtonLink onClick={() => open && onNavClick(!open)} href='#hireMe' dark={!open}>
+                Hire me
+            </ButtonLink>
         </HeaderContainer>
     );
 };
