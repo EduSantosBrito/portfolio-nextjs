ecc0091099ce4132e9831c827adde7f3950837e1
EduSantosBrito <edu.santos.brito@gmail.com>
2020-10-04 20:35:28 -0300

    feat: include showcase

---
+++ components/Showcase/layout.tsx
@@ -0,0 +1,33 @@
+import { CardsContainer, HorizontalScrollContainer } from '@components/Languages/style';
+import { ShowcaseItem } from '@types';
+import React from 'react';
+import { Card, CardDescription, CardTitle } from 'shared/Card';
+import { SectionTitle } from 'shared/SectionTitle';
+import { ShowcaseContainer, ShowcaseLink } from './style';
+
+interface ShowcaseLayoutProps {
+    items: ShowcaseItem[];
+}
+
+const ShowcaseLayout = ({ items }: ShowcaseLayoutProps) => {
+    return (
+        <ShowcaseContainer id='showcase'>
+            <SectionTitle>Showcase</SectionTitle>
+            <CardsContainer>
+                <HorizontalScrollContainer>
+                    {items.map(item => (
+                        <Card key={item.title}>
+                            <CardTitle>{item.title}</CardTitle>
+                            <CardDescription>{item.description}</CardDescription>
+                            <ShowcaseLink href={item.url} target='_blank'>
+                                See more
+                            </ShowcaseLink>
+                        </Card>
+                    ))}
+                </HorizontalScrollContainer>
+            </CardsContainer>
+        </ShowcaseContainer>
+    );
+};
+
+export default ShowcaseLayout;
