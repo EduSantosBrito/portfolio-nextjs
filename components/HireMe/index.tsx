93f4ce5532cc72faf587c45c0a4b36e489cdaf2f
EduSantosBrito <edu.santos.brito@gmail.com>
2020-10-04 21:00:26 -0300

    feat: create HireMe component

---
+++ components/HireMe/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HireMeContainer, HireMeTitle, HireMeEmail } from './styled';
+
+const HireMe = () => {
+    return (
+        <HireMeContainer id='hireMe'>
+            <HireMeTitle>Let&apos;s talk</HireMeTitle>
+            <HireMeEmail href='mailto:hi@brito.top'>hi@brito.top</HireMeEmail>
+        </HireMeContainer>
+    );
+};
+
+export default HireMe;
