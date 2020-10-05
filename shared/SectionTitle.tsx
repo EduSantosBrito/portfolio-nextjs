0ed3313329efec3ee9c2c87d7fe2c1609b58254e
EduSantosBrito <edu.santos.brito@gmail.com>
2020-10-04 21:04:38 -0300

    fix: disable user-select

--- shared/SectionTitle.tsx
+++ shared/SectionTitle.tsx
@@ -13,6 +13,14 @@ export const SectionTitle = styled.h2<SectionTitleProps>`
     z-index: 2;
     margin: 32px 0;
 
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+
     &::after {
         content: '';
         width: 100%;
