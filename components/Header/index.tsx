bdbbd6cf78eadccd43c6b27efa461342d49ed02a
EduSantosBrito <edu.santos.brito@gmail.com>
2020-10-06 13:17:36 -0300

    feat: desktop Header style

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { HeaderContainer, HeroText, HeroTextOrange } from './style';
+import { HeaderContainer, HeroDraw, HeroText, HeroTextContainer, HeroTextOrange } from './style';
 
 const Header = () => {
     return (
         <HeaderContainer id='header'>
-            <HeroText>
-                Hello I&apos;m <HeroTextOrange>Brito</HeroTextOrange>
-            </HeroText>
-            <HeroText>and I&apos;m a</HeroText>
-            <HeroText>Web developer</HeroText>
+            <HeroTextContainer>
+                <HeroText>
+                    Hello I&apos;m <HeroTextOrange>Brito</HeroTextOrange>
+                </HeroText>
+                <HeroText>and I&apos;m a</HeroText>
+                <HeroText>Web developer</HeroText>
+            </HeroTextContainer>
+            <HeroDraw alt='Draw of a programmer using a computer' />
         </HeaderContainer>
     );
 };
--- components/Header/style.tsx
+++ components/Header/style.tsx
@@ -1,7 +1,9 @@
 import { WithTheme } from 'shared/withTheme';
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.header`
+type HeroContainerProps = WithTheme;
+
+export const HeaderContainer = styled.header<HeroContainerProps>`
     position: relative;
     display: flex;
     flex-direction: column;
@@ -20,6 +22,34 @@ export const HeaderContainer = styled.header`
         position: absolute;
         left: 0;
         bottom: -50px;
+        @media (min-width: ${({ theme }: HeroContainerProps) => theme.breakpoints.sm}) {
+            width: 150px;
+            bottom: 24px;
+        }
+
+        @media (min-width: ${({ theme }: HeroContainerProps) => theme.breakpoints.lg}) {
+            width: 100%;
+            bottom: 0;
+        }
+    }
+
+    @media (min-width: ${({ theme }: HeroContainerProps) => theme.breakpoints.sm}) {
+        flex-direction: row;
+        justify-content: space-evenly;
+    }
+`;
+
+type HeroTextContainerProps = WithTheme;
+
+export const HeroTextContainer = styled.div<HeroTextContainerProps>`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    @media (min-width: ${({ theme }: HeroTextContainerProps) => theme.breakpoints.sm}) {
+        align-items: flex-start;
+        justify-content: flex-start;
     }
 `;
 
@@ -35,6 +65,17 @@ type HeroTextOrangeProps = WithTheme;
 
 export const HeroTextOrange = styled.span<HeroTextOrangeProps>`
     font-weight: 600;
-    font-size: ${({ theme }: HeroTextProps) => theme.fontSizes.extraLarge};
+    font-size: ${({ theme }: HeroTextOrangeProps) => theme.fontSizes.extraLarge};
     color: ${({ theme }: HeroTextOrangeProps) => theme.colors.secondary.main};
+`;
+
+type HeroDrawProps = WithTheme;
+
+export const HeroDraw = styled.img<HeroDrawProps>`
+    content: url('/header-draw.svg');
+    display: none;
+
+    @media (min-width: ${({ theme }: HeroDrawProps) => theme.breakpoints.sm}) {
+        display: block;
+    }
 `;
